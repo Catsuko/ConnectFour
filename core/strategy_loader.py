8d0690d8fb4979fe83be1fd37242dc4c811e4d7b
p3pping <mikepepping@gmail.com>
2019-10-23 19:43:16 +1000

    find_and_load now clears before searching

--- core/strategy_loader.py
+++ core/strategy_loader.py
@@ -10,6 +10,7 @@ class StrategyLoader:
         self.strategies_dir = strategies_dir
 
     def find_and_load(self):
+        self.strategies.clear()
         python_files = [file for file in listdir(self.strategies_dir) if isfile(join(self.strategies_dir, file)) and re.search("[a-zA-Z_0-9]+[\.][p][y]",file)]
         [self._import_from_file(file) for file in python_files if file != "__init__.py"]
 
