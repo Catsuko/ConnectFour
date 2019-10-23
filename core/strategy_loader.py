c348737ddab327d6c29a204118989c34142de8aa
p3pping <mikepepping@gmail.com>
2019-10-23 19:46:48 +1000

    Raise exception if less than 2 strategies were found

--- core/strategy_loader.py
+++ core/strategy_loader.py
@@ -1,7 +1,10 @@
 from os import listdir
 from os.path import isfile, join, basename
 import re
-from importlib import import_module    
+from importlib import import_module
+
+class InsufficientStrategiesError(Exception):
+    pass
 
 class StrategyLoader:
     
@@ -13,6 +16,8 @@ class StrategyLoader:
         self.strategies.clear()
         python_files = [file for file in listdir(self.strategies_dir) if isfile(join(self.strategies_dir, file)) and re.search("[a-zA-Z_0-9]+[\.][p][y]",file)]
         [self._import_from_file(file) for file in python_files if file != "__init__.py"]
+        if len(self) < 2:
+            raise InsufficientStrategiesError("Less that two strategies were found.")
 
     def _import_from_file(self, file_name):
         file = open(join(self.strategies_dir, file_name))
