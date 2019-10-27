d2f8fe1c138a21817e99f1dd69d40e04ebf99220
Sherry <Shewwiii@users.noreply.github.com>
2019-10-28 00:01:58 +1000

    wrote strategy

---
+++ strategies/sherry_strategy.py
@@ -0,0 +1,15 @@
+from core.strategy import Strategy
+
+class SherryStrategy(Strategy):
+    blocked = [0] * 1
+
+    def place_token(self, token, board):
+        row_to_check = board[3]
+        for x in row_to_check:
+            if row_to_check[x] != 0 and self.blocked[x] == 0:
+                self.blocked[x] = 1
+                return x
+        return 4
+
+def export_strategy():
+    return SherryStrategy()
