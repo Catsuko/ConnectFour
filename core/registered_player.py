f410284fe5457db642c70019b5beb04a67ca760f
Lewis Reid <lreidwilliams@gmail.com>
2019-11-01 21:03:00 +1000

    implemented registered player for easy strategy pickin

---
+++ core/registered_player.py
@@ -0,0 +1,29 @@
+class RegisteredPlayer:
+
+    def __init__(self, strategies):
+        self.strategies = strategies
+        self.strategy_index = None
+        self.name = None
+
+    def place_token(self, token, board):
+        return self.selected_strategy().place_token(token, board)
+
+    def selected_strategy(self):
+        if self.strategy_index is None:
+            print("Which strategy would you like to use?")
+            i = 0
+            for strategy in self.strategies:
+                print("%d: %s" % (++i, strategy))
+            self.strategy_index = int(input()) - 1
+        return self.strategies[self.strategy_index]
+
+    def selected_name(self):
+        if self.name is None:
+            print("What is your name?")
+            self.name = str(input())
+        return self.name
+    
+    def __str__(self):
+        return "%s (%s)" % (self.selected_name(), self.selected_strategy())
+
+    
--- core/strategy.py
+++ core/strategy.py
@@ -2,3 +2,6 @@ class Strategy:
 
     def place_token(self, token, board):
         raise NotImplementedError("Implement this method in your subclass.")
+
+    def __str__(self):
+        return self.__class__.__name__
