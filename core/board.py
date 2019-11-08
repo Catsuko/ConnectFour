89e56433ac9d4931167a3f9b4f419483c76f7c39
Lewis Reid <lreidwilliams@gmail.com>
2019-11-09 00:53:44 +1000

    added longest line length method to the board

--- core/board.py
+++ core/board.py
@@ -1,4 +1,5 @@
 from .errors import TokenPlacementError
+import math
 
 class Board:
     
@@ -31,3 +32,39 @@ class Board:
                     arr[y][self.moves[i]] = (i % 2) + 1
                     break
         return arr
+
+    def longest_line_length(self):
+        grid = self.to_array()
+        series = []
+        series.extend(grid)
+        for x in range(self.width):
+            series.append([grid[y][x] for y in range(self.height)])
+            series.append([grid[pos[1]][pos[0]] for pos in self.line(x, 0, 1, 1)])
+            series.append([grid[pos[1]][pos[0]] for pos in self.line(x, self.height - 1, 1, -1)])
+        for y in range(1, self.height):
+            series.append([grid[pos[1]][pos[0]] for pos in self.line(0, y, 1, 1)])
+            series.append([grid[pos[1]][pos[0]] for pos in self.line(0, self.height - y, 1, -1)])
+            
+        return max([self.longest_run_length(s) for s in series])
+
+    def line(self, x, y, xDir, yDir):
+        line = []
+        while 0 <= x < self.width and 0 <= y < self.height:
+            line.append((x,y))
+            x = x + xDir
+            y = y + yDir
+        return line
+        
+    
+    def longest_run_length(self, series):
+        lines = [[]]
+        for i in range(len(series)):
+            if i == 0 or series[i] != series[i-1]:
+                lines.append([])
+            lines[-1].append(series[i])
+        return len(sorted(list(filter(lambda n: len(n) == 0 or n[0] > 0, lines)), key=lambda l: -len(l))[0])
+            
+        
+            
+            
+            
--- core/connect_four_game.py
+++ core/connect_four_game.py
@@ -2,52 +2,32 @@ from .score import Score
 
 class ConnectFourGame:
     
-    def __init__(self, board):
+    def __init__(self, board, win_threshold=4):
         self.board = board
+        self.win_threshold = win_threshold
 
-    # TODO: Refine the return result into something more useful
     def play(self, player1, player2, view):
         turn = 0
         players = [player1, player2]
         board = self.board.fresh()
         view.print_start(player1, player2)
         view.print_board(board.to_array())
-        # TODO: Catch errors that occur during a turn and auto lose the player that made the error
         while not board.is_full():
             # TODO: Token object? token.other and token.value would be useful methods
             current_token = (turn % 2) + 1
             current_player = players[current_token - 1]
-            # TODO: Pass board to player and provide helpful methods instead of 2d array
             desired_column = current_player.place_token(current_token, board.to_array())
             try:
                 board = board.place_token(desired_column)
                 board_array = board.to_array()
                 view.print_board(board_array)
+                turn = turn + 1
             except Exception as error:
                 winner = players[((turn+1) % 2)]
                 view.print_result("%s won! %s caused a %s" % (winner, current_player, error.__class__.__name__))
                 return (winner, Score(1, 0, 0))
-            # TODO: Move this into Board
-            for y in range(len(board_array)):
-                for x in range(len(board_array[y])):
-                    if self.check_for_four(x, y, board_array):
-                        view.print_result("%s got 4!" % current_player)
-                        return (current_player, Score(1, 0, 0))
-            
-            turn = turn + 1
+            if board.longest_line_length() >= self.win_threshold:
+                view.print_result("%s got %s!" % (current_player, self.win_threshold))
+                return (current_player, Score(1, 0, 0))       
         view.print_result("Nobody won!")
         return (None, Score(0, 1, 0))
-
-    # TODO: Clean this up!
-    def check_for_four(self, x, y, board):
-        token = board[y][x]
-        if token > 0:
-            if x >= 3 and all(token == t for t in (board[y][x-1], board[y][x-2], board[y][x-3])):
-                return True
-            if y >= 3 and all(token == t for t in (board[y-1][x], board[y-2][x], board[y-3][x])):
-                return True
-            if x >=3 and y >= 3 and all(token == t for t in (board[y-1][x-1], board[y-2][x-2], board[y-3][x-3])):
-                return True
-            if x <= len(board[y]) - 4 and y >= 3 and all(token == t for t in (board[y-1][x+1], board[y-2][x+2], board[y-3][x+3])):
-                return True
-        return False
