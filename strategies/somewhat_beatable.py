2a13bc53a4a5eba78834a7790203d4b654a802f7
Sherry <Shewwiii@users.noreply.github.com>
2019-11-11 01:57:35 +1000

    check new game

---
+++ strategies/somewhat_beatable.py
@@ -0,0 +1,44 @@
+from core.strategy import Strategy
+
+class SomewhatBeatable(Strategy):
+    blocked = [0] * 7
+    # pos = 3
+    priority = [3,4,5,2,1,6,0]
+
+
+    def place_token(self, token, board):
+
+        #new game
+        if board[-1].count(0) > 5:
+            self.blocked = [0] * 7
+
+        row_to_check = board[3]
+        top_row = board[0]
+        # print(self.blocked)
+        # print(row_to_check)
+        for x in range(len(row_to_check)):
+            # print(row_to_check[x])
+            if row_to_check[x] != 0 and self.blocked[x] == 0:
+                self.blocked[x] = 1
+                return x
+        # print(board[0][self.pos])
+        
+        # if board[0][self.pos] == 0:
+        for x in self.priority:
+            if board[0][x] == 0:
+                # self.pos = x
+                return x
+
+    # def new_game(board):
+    #     if board[-1].count(0) > 5:
+    #         self.blocked = [0] * 7
+
+    def can_i_win():
+        return 0
+
+    def is_there_threat():
+        return 0
+            
+
+def export_strategy():
+    return SomewhatBeatable()
