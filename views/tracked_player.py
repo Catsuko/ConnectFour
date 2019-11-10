0cc614e459c03974187efb07a8f7f3e93d337d05
Lewis Reid <lreidwilliams@gmail.com>
2019-11-10 19:13:06 +1000

    added training dummy script for viewing algorithms

--- views/tracked_player.py
+++ views/tracked_player.py
@@ -1,11 +1,10 @@
-import time
 from .tracked_grid import TrackedGrid
 
 class TrackedPlayer:
 
-    def __init__(self, player, delay=1):
+    def __init__(self, player, view):
         self.player = player
-        self.delay = delay
+        self.view = view
 
     def __str__(self):
         return "%s (Tracked)"  % self.player
@@ -13,17 +12,8 @@ class TrackedPlayer:
     def place_token(self, token, board):
         tracked_board = TrackedGrid(board)
         chosen_column = self.player.place_token(token, tracked_board)
-
         positions = tracked_board.read_all()
         for pos in positions:
-            grid = ''
-            for y in range(len(board)):
-                for x in range(len(board[y])):
-                    grid = grid + ('X' if x == pos[0] and y == pos[1] else '_')
-                grid = grid + '\n'
-            print('%s\r' % grid)
-            time.sleep(self.delay)
-        print()
-
+            self.view.print_access(pos, board)
         return chosen_column
                     
